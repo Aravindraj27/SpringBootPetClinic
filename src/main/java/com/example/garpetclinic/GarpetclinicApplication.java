c3f4c7c164be79d47450ae790d497fec937e7d18
Aravindraj_27 <aravind96raj@outlook.com>
2021-12-26 22:27:12 +0530

    Maven Dependencies Initialised using start.spring.io

---
+++ src/main/java/com/example/garpetclinic/GarpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.garpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarpetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarpetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/garpetclinic/GarpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarpetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
